fe0ad23db220a8d200b581714b84e375e4573bed
Finn Lancaster <flancast90@gmail.com>
2021-05-08 17:13:08 -0400

    Update script.js

--- Resume/Assets/js/script.js
+++ Resume/Assets/js/script.js
@@ -1,10 +1,10 @@
 window.addEventListener('load',function(){
 var ot = parseInt(document.getElementById('about').offsetTop,10);
-document.getElementById('about-tag').style.marginTop=ot+150;
+document.getElementById('about-tag').style.top=ot+150;
 
 var ot2 = parseInt(document.getElementById('projects').offsetTop,10);
-document.getElementById('project-tag').style.marginTop=ot2+250;
+document.getElementById('project-tag').style.top=ot2+250;
 
 var ot3= parseInt(document.getElementById('links').offsetTop,10);
-document.getElementById('link-tag').style.marginTop=ot3+250;
+document.getElementById('link-tag').style.top=ot3+250;
 });
